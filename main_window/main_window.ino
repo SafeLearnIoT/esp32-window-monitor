6e765361c4a2f6a256d6ccf24c218b7f0b1f1fbd
Mikołaj Macura <mikolajmacura0510@gmail.com>
2024-03-18 22:46:28 +0100

    add schematics + change comm

--- main_window/main_window.ino
+++ main_window/main_window.ino
@@ -1,4 +1,14 @@
-#include "globals.h"
+#include "env.h"
+#include "ArduinoJson.h"
+#include <communication.h>
+
+const char ssid[] = SSID_ENV;
+const char pass[] = PASSWORD_ENV;
+
+const char mqtt_host[] = MQTT_HOST_ENV;
+const int mqtt_port = MQTT_PORT_ENV;
+
+JsonDocument data;
 
 #define BLINDS_1_PIN  19
 #define BLINDS_2_PIN 21
@@ -12,72 +22,65 @@ int blinds_1_state;
 int blinds_2_state;
 int window_state;
 
+
+void callback(String &topic, String &payload){
+  Serial.print("Topic: ");
+  Serial.print(topic);
+  Serial.print(", Payload: ");
+  Serial.println(payload);
+}
+
+Communication comm(SSID_ENV, PASSWORD_ENV, "/esp32/", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
+
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  mqtt_setup();
   pinMode(BLINDS_1_PIN, INPUT_PULLUP);
   pinMode(BLINDS_2_PIN, INPUT_PULLUP);
   pinMode(WINDOW_PIN, INPUT_PULLUP);
-
   pinMode(BLINDS_1_LED, OUTPUT);
   pinMode(BLINED_2_LED, OUTPUT);
   pinMode(WINDOW_LED, OUTPUT);
+  Serial.println("wow");
+  comm.setup();
 }
 
 void loop() {
-  mqtt_client.loop();
-  delay(10);
-
-  if (!mqtt_client.connected()) {
-    mqtt_connect();
-  }
+  comm.handle_mqtt_loop();
 
   blinds_1_state = digitalRead(BLINDS_1_PIN);
   blinds_2_state = digitalRead(BLINDS_2_PIN);
   window_state = digitalRead(WINDOW_PIN);
 
-  String topic = "/esp32/";
-  topic += TYPE;
+  data.clear();
   
-  String output = "{ \"blinds_1\": \"";
-  
+  data["time"] = millis();
+
   if (blinds_1_state == HIGH) {
-    output += "open";
-    Serial.print("[Blinds 1 open | ");
+    data["blinds_1"] = "open";
     digitalWrite(BLINDS_1_LED, HIGH);
   } else {
-    output += "closed";
-    Serial.print("[Blinds 1 closed | ");
+    data["blinds_1"] = "closed";
     digitalWrite(BLINDS_1_LED, LOW);
   }
-
-  output += "\", \"blinds_2\": \"";
  
   if (blinds_2_state == HIGH) {
-    output += "open";
-    Serial.print("Blinds 2 open | ");
+    data["blinds_2"] = "open";
     digitalWrite(BLINED_2_LED, HIGH);
   } else {
-    output += "closed";
-    Serial.print("Blinds 2 closed | ");
+    data["blinds_2"] = "closed";
     digitalWrite(BLINED_2_LED, LOW);
   }
 
-  output += "\", \"window\": \"";
-
   if (window_state == HIGH) {
-    output += "open";
-    Serial.println("Window open]");
+    data["window"] = "open";
     digitalWrite(WINDOW_LED, HIGH);
   } else {
-    output += "closed";
-    Serial.println("Window closed]");
+    data["window"] = "closed";
     digitalWrite(WINDOW_LED, LOW);
   }
 
-  output += "\" }";
-  mqtt_client.publish(topic.c_str(), output.c_str());
+  String output;
+  serializeJson(data, output);
+  comm.publish("window_sensor", output);
   delay(5000);
 }
