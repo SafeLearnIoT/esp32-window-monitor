9f184697014cef83b85f2e4acc74a0e50cb9acda
Mikołaj Macura <mikolajmacura0510@gmail.com>
2024-03-19 19:44:40 +0100

    switch from Arduino IDE to PlatformIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include <Arduino.h>
+
 #include "env.h"
 #include "ArduinoJson.h"
-#include <communication.h>
+#include "communication.h"
 
 const char ssid[] = SSID_ENV;
 const char pass[] = PASSWORD_ENV;
@@ -10,7 +12,7 @@ const int mqtt_port = MQTT_PORT_ENV;
 
 JsonDocument data;
 
-#define BLINDS_1_PIN  19
+#define BLINDS_1_PIN 19
 #define BLINDS_2_PIN 21
 #define WINDOW_PIN 18
 
@@ -22,8 +24,8 @@ int blinds_1_state;
 int blinds_2_state;
 int window_state;
 
-
-void callback(String &topic, String &payload){
+void callback(String &topic, String &payload)
+{
   Serial.print("Topic: ");
   Serial.print(topic);
   Serial.print(", Payload: ");
@@ -32,19 +34,24 @@ void callback(String &topic, String &payload){
 
 Communication comm(SSID_ENV, PASSWORD_ENV, "/esp32/", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+
   pinMode(BLINDS_1_PIN, INPUT_PULLUP);
   pinMode(BLINDS_2_PIN, INPUT_PULLUP);
   pinMode(WINDOW_PIN, INPUT_PULLUP);
   pinMode(BLINDS_1_LED, OUTPUT);
   pinMode(BLINED_2_LED, OUTPUT);
   pinMode(WINDOW_LED, OUTPUT);
-  Serial.println("wow");
+
+  while (!Serial)
+    ;
   comm.setup();
 }
 
-void loop() {
+void loop()
+{
   comm.handle_mqtt_loop();
 
   blinds_1_state = digitalRead(BLINDS_1_PIN);
@@ -52,29 +59,38 @@ void loop() {
   window_state = digitalRead(WINDOW_PIN);
 
   data.clear();
-  
+
   data["time"] = millis();
 
-  if (blinds_1_state == HIGH) {
+  if (blinds_1_state == HIGH)
+  {
     data["blinds_1"] = "open";
     digitalWrite(BLINDS_1_LED, HIGH);
-  } else {
+  }
+  else
+  {
     data["blinds_1"] = "closed";
     digitalWrite(BLINDS_1_LED, LOW);
   }
- 
-  if (blinds_2_state == HIGH) {
+
+  if (blinds_2_state == HIGH)
+  {
     data["blinds_2"] = "open";
     digitalWrite(BLINED_2_LED, HIGH);
-  } else {
+  }
+  else
+  {
     data["blinds_2"] = "closed";
     digitalWrite(BLINED_2_LED, LOW);
   }
 
-  if (window_state == HIGH) {
+  if (window_state == HIGH)
+  {
     data["window"] = "open";
     digitalWrite(WINDOW_LED, HIGH);
-  } else {
+  }
+  else
+  {
     data["window"] = "closed";
     digitalWrite(WINDOW_LED, LOW);
   }
