97dae8f880fa9658c81817ba896ab75d280e6da2
mikomac405 <mikolajmacura0510@gmail.com>
2025-02-19 19:19:32 +0100

    windows ver

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+#include "jsonToArray.h"
 #include "env.h"
 #include "ml.h"
 #include "communication.h"
@@ -16,20 +17,25 @@ void callback(String &topic, String &payload);
 
 auto comm = Communication::get_instance(SSID_ENV, PASSWORD_ENV, "esp32/window", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
 
+bool get_weights = false;
+bool set_weights = false;
+String payload_content;
+
 void callback(String &topic, String &payload)
 {
     Serial.println("[SUB][" + topic + "] " + payload);
     if(payload == "get_weights")
     {
-        comm->hold_connection();
-        comm->publish("cmd", "", true);
-        auto weights = blinds_1_ml.get_weights();
-        serializeJson(weights, Serial);
-        Serial.println();
+        if(!get_weights){
+            get_weights = true;
+        }
     }
-    if(payload=="set_weights")
+    if(payload.startsWith("set_weights;"))
     {
-        comm->release_connection();
+       if(!set_weights){
+           payload_content = payload;
+           set_weights = true;
+       }
     }
 }
 
@@ -49,18 +55,61 @@ void setup()
   pinMode(WINDOW_PIN, INPUT_PULLUP);
 
   comm->setup();
+
+  delay(5000);
 }
 
 void loop()
 {
-  if (millis() - lastStateCheck > 60000)
-  {
-    lastStateCheck = millis();
-    if (blinds_1_state == digitalRead(BLINDS_1_PIN) && blinds_2_state == digitalRead(BLINDS_2_PIN) && window_state == digitalRead(WINDOW_PIN))
+  if(get_weights)
     {
-      return;
+        comm->hold_connection();
+
+        JsonDocument doc;
+        blinds_1_ml.get_weights(doc);
+        blinds_2_ml.get_weights(doc);
+        window_ml.get_weights(doc);
+        comm->publish("cmd_mcu", "weights;" + doc.as<String>());
+        get_weights = false;
+    }
+    if(set_weights)
+    {   
+        JsonDocument doc;
+        if(payload_content.substring(12)=="ok"){
+            comm->publish("cmd_gateway", "", true);
+            comm->release_connection();
+            payload_content.clear();
+            set_weights = false;
+            return;
+        }
+        deserializeJson(doc, payload_content.substring(12));
+        
+        auto blinds_1_weights = Converter<std::array<double, 4>>::fromJson(doc["blinds_1"]);
+        Serial.print("[Blinds 1]");
+        blinds_1_ml.set_weights(blinds_1_weights);
+
+        auto blinds_2_weights = Converter<std::array<double, 4>>::fromJson(doc["blinds_2"]);
+        Serial.print("[Blinds 2]");
+        blinds_2_ml.set_weights(blinds_2_weights);
+
+        auto window_weights = Converter<std::array<double, 4>>::fromJson(doc["window"]);
+        Serial.print("[Window]");
+        window_ml.set_weights(window_weights);
+        
+        // Clear message
+        comm->publish("cmd_gateway", "", true);
+
+        comm->release_connection();
+        payload_content.clear();
+        set_weights = false;
     }
 
+  comm->handle_mqtt_loop();
+  if (millis() - lastStateCheck > 900000)
+  {
+    comm->resume_communication();
+    
+    lastStateCheck = millis();
     auto raw_time = comm->get_rawtime();
 
     blinds_1_state = digitalRead(BLINDS_1_PIN);
@@ -76,21 +125,24 @@ void loop()
     detail_sensor_data["window"] = window_state;
 
    
-    blinds_1_ml.perform(blinds_1_state);
-    blinds_2_ml.perform(blinds_2_state);
-    window_ml.perform(window_state);
-    
-    serializeJson(sensor_data, Serial);
-    Serial.println();
-  
-    comm->send_data(sensor_data);
-  }
+    JsonDocument ml_data;
+    ml_data["time_sent"] = raw_time;
+    ml_data["device"] = comm->get_client_id();
+    JsonObject detail_ml_data = ml_data["data"].to<JsonObject>();
 
-  if (millis() - lastHealthPing > 900000)
-  {
-    lastHealthPing = millis();
-    comm->resume_communication();
-    delay(5000);
-    comm->pause_communication();
+    JsonObject blinds_1_data = detail_ml_data["blinds_1"].to<JsonObject>();
+    JsonObject blinds_2_data = detail_ml_data["blinds_2"].to<JsonObject>();
+    JsonObject window_data = detail_ml_data["window"].to<JsonObject>();
+    blinds_1_ml.perform(blinds_1_state, blinds_1_data);
+    blinds_2_ml.perform(blinds_2_state, blinds_2_data);
+    window_ml.perform(window_state, window_data);
+    
+    //serializeJson(sensor_data, Serial);
+    //Serial.println();
+    //comm->send_data(sensor_data);
+
+    serializeJson(ml_data, Serial);
+    Serial.println();
+    comm->send_ml(ml_data);
   }
 }
